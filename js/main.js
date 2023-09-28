3e105a0f2d778047708583830fa81829e984afcc
Ainhoadlhs <ainhoasilveira@gmail.com>
2023-09-28 18:13:43 +0200

    firt js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const input = document.querySelector('.js-input');
+const search = document.querySelector('.js-btn');
+
+const series = document.querySelector('.js-series');
+const favorites = document.querySelector('.js-favs');
