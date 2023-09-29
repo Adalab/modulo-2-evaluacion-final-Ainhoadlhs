ec9fad5d107a65e99db6593f460e0f325da5ac2c
Ainhoadlhs <ainhoasilveira@gmail.com>
2023-09-29 13:25:01 +0200

    i am going to rest in peace

--- js/main.js
+++ js/main.js
@@ -1,40 +1,81 @@
 "use strict";
 
+/* Variables */
 const input = document.querySelector('.js-input');
-const search = document.querySelector('.js-btn');
+const searchBtn = document.querySelector('.js-btn');
 const messageError = document.querySelector('.js-msg-error')
 
 const series = document.querySelector('.js-series');
 const favorites = document.querySelector('.js-favs');
 
+/*Arrays*/
 let seriesList = [];
 let seriesFav = [];
 
-function searchSerie (event) {
-    event.preventDefault ();
-    let drawApi =  '';
 
-    const name = input.value;
-    // console.log(name);
-        fetch(`//api.tvmaze.com/search/shows?q=${name}`)
-        .then((response) => response.json())
-        .then((data) => {
-            seriesList = data;
-            if (data.length === 0) {
-                messageError.innerHTML = 'No hay series para mostrar';
-            } else {
-                messageError.innerHTML = ''; // En caso de que haya datos, borra el mensaje anterior
-            }
-        })
+/* Pintar lista de series*/
+function renderSeriesList(listSeries) {
+    series.innerHTML = '';
+    for (const oneSerie of listSeries) {
+        let drawSerie = '<div class="clickSerie">';
+        drawSerie += `<h2 class="title">${oneSerie.show.name}</h2>`;
+        if(oneSerie.show.image === null)
+        {
+            drawSerie += '<img class="serieImg" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV">';
+        }else{
+            drawSerie += `<img class="serieImg" src=${oneSerie.show.image.medium}>`;
+        }
+        drawSerie += '</div>';
+        series.innerHTML += drawSerie;
+    }
+        /* Como al empezar el JS no existe la clase 'clickSerie' lo que hacemos aquí es añadirle el click */
+        const allDivs = document.querySelectorAll('.clickSerie');
 
-        seriesList.forEach((serie) => {
-            // console.log(serie);
-            drawApi+=`<div class="title"> ${serie.name} </div>
-            <img src="${serie.url}" class="customImage">`;
-        })
-        //console.log(drawApi);
-        series.innerHTML = drawApi;
-
+        for(const oneDiv of allDivs) {
+            oneDiv.addEventListener('click', addFavSerie);
+        }
 }
 
-search.addEventListener('click', searchSerie);
+/* Relleno el array de series favoritas y lo pinto */
+function addFavSerie() {
+    const name = this.childNodes[0].innerHTML;
+    const imgSerie = this.childNodes[1].src;
+    const dataSerie = {
+        'name' : name,
+        'image' : imgSerie,
+    };
+    seriesFav.push(dataSerie);
+    renderSeriesFavs();
+}
+
+/* Pintar series favoritas*/
+function renderSeriesFavs() {
+    favorites.innerHTML = "";
+    for (const favSerie of seriesFav) {
+        let drawSerie = '<div>';
+        drawSerie += `<h2 class="title">${favSerie.name}</h2>`;
+        drawSerie += `<img class="serieImg" src=${favSerie.image}>`;
+        drawSerie += '</div>';
+        favorites.innerHTML += drawSerie;
+    }
+}
+
+/* Evento para buscar las series en la api y las pinta */
+function handleSearch(event) {
+    event.preventDefault()
+
+    const name = input.value;
+    
+    fetch(`//api.tvmaze.com/search/shows?q=${name}`)
+    .then((response) => response.json())
+    .then((dataAPI) => {
+        seriesList = dataAPI;
+        localStorage.setItem('show', JSON.stringify(seriesList));
+        renderSeriesList(dataAPI);
+    });
+}
+
+/* Ejecutar el evento click al dar al botón de buscar */
+searchBtn.addEventListener('click', handleSearch);
+
+
